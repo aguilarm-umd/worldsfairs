2befc7b2cb90bac62d63c7e9f0675bd44ce5b95f
Ben Wallberg <wallberg@umd.edu>
2024-07-28 07:36:55 -0400

    Add detail pages to wget.sh

--- wget.sh
+++ wget.sh
@@ -5,12 +5,5 @@ wget \
   --convert-links \
   --page-requisites \
   --adjust-extension \
-  --input-file=- << 'END'
-https://digital.lib.umd.edu/worldsfairs
-https://digital.lib.umd.edu/worldsfairs/about
-https://digital.lib.umd.edu/worldsfairs/essays
-https://digital.lib.umd.edu/worldsfairs/exhibits
-https://digital.lib.umd.edu/worldsfairs/documentation
-https://digital.lib.umd.edu/worldsfairs/rights
-https://digital.lib.umd.edu/worldsfairs/browse
-END
+  --input-file=wget-urls.txt
+
